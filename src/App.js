34aa258f82379c0ef74a328bb83c8050a996d116
Delvin <delvinfernandezm@gmail.com>
2021-03-16 17:03:38 -0500

    agregado el nombre de persona
    enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -24,21 +24,65 @@ class App extends React.Component {
   
 
   render() {
+    
+
     const items = this.state.toDoList.map(i => <li>{i}</li>)
     return (
       <div>
+        <Hola myName={"Don Gil"} />
+  
         <textarea onChange={this.handleChange} value={this.state.userInput} placeholder="Separa con comas (,)" />
         <br />
         <button onClick={this.handleSubmit}>Crear Lista</button>
         <h1>Mi lista de tareas</h1>
         <ul>{items} </ul>
 
+       
 
       </div>
 
+    
+
     )
   }
+
+  
 }
+
+
+class Hola extends React.Component {
+constructor(props){super(props);
+
+  this.state = {
+    show: true,
+  
+  };
+  
+  this.toggleShow = this.toggleShow.bind(this);
+}
+
+toggleShow () {
+  this.setState({show: !this.state.show})};
+
+  render() { 
+    if(this.state.show){
+    return(
+    <div>
+    <h1> Hola {this.props.myName}<button onClick={this.toggleShow}>Ocultar Nombre</button> </h1> 
+    </div>
+    )}
+    
+    else{ return(
+<div>
+    <h1> Hola Anonimo <button onClick={this.toggleShow}>Ver Nombre</button></h1> 
+    </div>
+    )}
+}
+ 
+}
+
+
+
 
 
 export default App;
